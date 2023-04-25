ff8eba4abaf1a97990c75a4deaf0336d4044920d
smmurad <muradsigurd@gmail.com>
2023-04-25 17:50:42 +0200

    Tutorial 5

--- tutorial.py
+++ tutorial.py
@@ -1,11 +1,26 @@
-import pandas as pd
-import yfinance as yf
-import matplotlib.pyplot as plt
-from matplotlib import style
-from pathlib import Path
-import ticker_util as tu
+import bs4 as bs
+import pickle
+import requests
 
-style.use('ggplot')
+## TUTORIAL 5 & 6
+
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+    
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+    
+    return tickers
+    
+print(save_sp500_tickers())
+
+
 
 ## TUTORIAL 4
 # import mplfinance as mpf
