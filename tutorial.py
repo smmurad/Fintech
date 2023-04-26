f7aec357dfe6d9e7b9b199751d69f86f8675eb03
smmurad <muradsigurd@gmail.com>
2023-04-26 11:42:33 +0200

    Tutorial 7 and 8

--- tutorial.py
+++ tutorial.py
@@ -1,17 +1,65 @@
-import bs4 as bs
+import numpy as np
 import pickle
 import requests
 import datetime as dt
 import os
 import pandas as pd
-import ticker_util as tu
-from pathlib import Path
 
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('ggplot')
 
+## TUTORIAL 8
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df_corr = df.corr(numeric_only=True)
+    
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    
+    coloumn_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax.set_xticklabels(coloumn_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1, 1)
+    plt.tight_layout()
+    plt.show()
+    
+## TUTORIAL 7
+def compile_data():
+    with open('sp500tickers.pickle', 'rb') as f:
+        tickers = pickle.load(f)
+    main_df = pd.DataFrame()
+    
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv(f'stock_dfs/{ticker}.csv')
+        df.set_index('Date', inplace=True)
+        df.rename(columns= {'Adj Close': ticker}, inplace=True)
+        df.drop(['Open','High','Close','Volume','Low', 'ticker'], axis=1, inplace=True)
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, on='Date')
+        
+        if count % 10 == 0:
+            print(count)
+            
+    main_df.to_csv('sp500_joined_closes.csv')
+        
+        
 ## TUTORIAL 5 & 6
 
-tu.get_data_from_yahoo()
+# tu.get_data_from_yahoo()
 
 ## TUTORIAL 4
 # import mplfinance as mpf
